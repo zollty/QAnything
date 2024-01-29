34bb143f50b5d5cc56d940ae1da17a95b0fada19
zollty <zollty@163.com>
2024-01-30 01:13:32 +0800

    u

--- front_end/src/components/Chat.vue
+++ front_end/src/components/Chat.vue
@@ -136,7 +136,7 @@ import { userId } from '@/services/urlConfig';
 const typewriter = new Typewriter((str: string) => {
   if (str) {
     QA_List.value[QA_List.value.length - 1].answer += str || '';
-    console.log("-------" + str)
+    console.log("-------" + str);
   }
 });
 
