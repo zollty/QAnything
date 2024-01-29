a7061ad65f1fe9d2f3f9a0c7a3f0a33e8df3f905
zollty <zollty@163.com>
2024-01-29 19:19:13 +0800

    update

--- front_end/.prettierrc.js
+++ front_end/.prettierrc.js
@@ -4,7 +4,7 @@ module.exports = {
   semi: true,
   bracketSpacing: true,
   arrowParens: 'avoid',
-  printWidth: 100,
+  printWidth: 1000,
   useTabs: false,
   tabWidth: 2,
   eslintIntegration: true, //让prettier使用eslint的代码格式进行校验
